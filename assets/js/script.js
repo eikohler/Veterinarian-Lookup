e4ad8a40551dcc9c8148e6b9035969abab451c6c
Eric Kohler <erickohler1600@gmail.com>
2022-08-09 17:40:11 -0400

    fetch geocode data by address api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
-const getGeo = (data) =>{
-
+const getGeo = async (address) =>{
+    const url = `https://geocode.search.hereapi.com/v1/geocode?q=${address}&apiKey=1NIWlBjL8kQJXRvc3Eeev6AB387ibOdL9qGoQhrskVw`;
+    let response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+    return await response.json();
 }
 
-$('#address-form').submit(function(event){
+$('#address-form').submit(async function(event){
     event.preventDefault();
     let address = $('#address').val();
     if(address){
         address = address.replace(/ /g, "+");
         address = address.replace(/,/g, "%2C");
         console.log(address);
+        const geo = await getGeo(address);
     }
 })
